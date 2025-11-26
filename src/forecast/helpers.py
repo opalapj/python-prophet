2ac115ce8e6904d8514c8e86eb43ad281ed83824
Piotr Opała <opalapj@gmail.com>
2025-11-26 10:30:09 +0100

    Add helpers

---
+++ src/forecast/helpers.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+
+def merge_spans(*spans):
+    # `spans`: two-elements tuples consists start and end date str in ISO 8601 format
+    # return: pd.DatetimeIndex
+    data = []
+    for span in spans:
+        span = [pd.Timestamp(ds) for ds in span]
+        data.append(
+            pd.date_range(
+                start=span[0],
+                end=span[1] + pd.DateOffset(days=1),
+                freq="h",
+                inclusive="left",
+            ).to_series()
+        )
+    span = pd.concat(data).index
+    return span
+
+
+def match_tz(df, tz):
+    # `df`: dataframe with naive DatetimeIndex
+    # `tz`: str, e.g. 'Europe/Warsaw'
+    # return: dataframe with aware DatetimeIndex
+    index = pd.date_range(
+        start=df.index.min(),
+        end=df.index.max(),
+        freq="h",
+        tz=tz,
+        name=df.index.name,
+    ).tz_localize(tz=None)
+    df = df.reindex(index=index)
+    df = df.tz_localize(
+        tz=tz,
+        ambiguous="infer",
+    )
+    return df
